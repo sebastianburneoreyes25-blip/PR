af3b330ecd83b484a93467fe672b0205abde996d
[sebastianburneoreyes25-blip] <[sebastian.burneoreyes25@campusfp.es]>
2025-10-06 17:24:05 +0200

    Se añade playlist ejercicio

---
+++ python/listaMusical.py
@@ -0,0 +1,14 @@
+playlist=[]
+pista=""
+while pista!="fin":
+    pista=input("Agrega el nombre de la canción a añadir a la playlis. Para terminar de añadir escribe en minusculas fin \n")
+    if pista!="fin":
+        playlist.append(pista)
+    else:
+        n=0
+        for cancion in playlist:
+            n=n+1
+            print(n, ". ", (cancion))
+m=int(input("Selecciona el numero de la cancion a reproducir. "))
+print(playlist[m])
+    
