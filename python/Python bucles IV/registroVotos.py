d62ffb0e4b53471855cf810b4080441a8f0e0f87
“[SebastianBurneo]” <sebastian.burneoreyes25@campusfp.es>
2025-10-23 13:10:14 +0200

    eso

---
+++ python/Python bucles IV/registroVotos.py
@@ -0,0 +1,18 @@
+#Definimos variables
+eleeciones={
+    "Sebas": 0,
+    "Taki":0,
+    "Desi":0,
+    "Lope":0
+
+}
+
+nombre=""
+#Definimos funciones
+
+def mostrarOpciones():
+
+def voto():
+
+    
+#Logica de programacion
