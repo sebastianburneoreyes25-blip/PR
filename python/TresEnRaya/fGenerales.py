6a98a090f67cf37e7f817a404f604dbcb05cbd84
“[SebastianBurneo]” <sebastian.burneoreyes25@campusfp.es>
2025-11-11 13:12:58 +0100

    debug:errores en resultados resuelto

--- python/TresEnRaya/fGenerales.py
+++ python/TresEnRaya/fGenerales.py
@@ -5,7 +5,7 @@ import pve
 import numpy as np
 #Definimos funciones
     
-def bienvenida(a):
+def bienvenida(a):#Funcion para dar la bienvenida y obtener que tipo de juego empezamos
     print("Bienvenido al 3 en raya. hay dos modalidades, PVP o PVE" )
     a=input("Para jugar PVP(personaVSpersona) presiona 0. Para jugar PVE(PersonaVSMaquina) presiona 1.\n")
     a=esNumerico(a)
@@ -15,13 +15,13 @@ def bienvenida(a):
 
     return a
 
-def esNumerico(a):
+def esNumerico(a):#Funcion para comprobar que es numero
     while a.isalpha():
         a=input("El comando debe ser un numero.\n")
     a=int(a)
     return a
 
-def tipoJuego(a,tablero,tableroJug,win1,win2):
+def tipoJuego(a,tablero,tableroJug,win1,win2):#En dependecia del juego se ejecuta un codigo u otro
     if a==0:
         while win1==False and win2==False and (tablero==0).any():
             win1,win2=pvp.pvp(tablero,tableroJug,win1,win2)
@@ -30,15 +30,15 @@ def tipoJuego(a,tablero,tableroJug,win1,win2):
             win1,win2=pve.pve(tablero,tableroJug,win1,win2)
     return win1,win2
 
-def comrpobarJ1(tablero):
+def comrpobarJ1(tablero):#Comprobamos si el J1 ha ganado
     win1=False
     diagonal=np.diag(tablero)
     diagonalInversa=np.fliplr(tablero).diagonal()
-    for i in range (3):
-        if (tablero[i-1,:].all()==1 ):
+    for i in range (2):
+        if (tablero[i,:].all()==1 ):
             win1=True
-    for i in range (3):
-        if (tablero[:,i-1].all()==1 ):
+    for i in range (2):
+        if (tablero[:,i].all()==1 ):
             win1=True 
     if (diagonal.all()==1):
         win1=True
@@ -47,15 +47,15 @@ def comrpobarJ1(tablero):
 
     return win1
 
-def comrpobarJ2(tablero):
+def comrpobarJ2(tablero):#Comprobamos si el J2 ha ganado
     win2=False
     diagonal=np.diag(tablero)
     diagonalInversa=np.fliplr(tablero).diagonal()
-    for i in range (3):
-        if (tablero[i-1,:].all()==2 ):
+    for i in range (2):
+        if (tablero[i,:].all()==2 ):
             win2=True
-    for i in range (3):
-        if (tablero[:,i-1].all()==2 ):
+    for i in range (2):
+        if (tablero[:,i].all()==2 ):
             win2=True
     if diagonal.all()==2:
         win2=True
@@ -64,7 +64,7 @@ def comrpobarJ2(tablero):
     
     return win2
 
-def resultado(tablero,win1,win2,tipo):
+def resultado(tablero,win1,win2,tipo):#Decimos si ha habido empate o hemos ganado
     if (tablero==0).any() and win1==False and win2==False:
         print("¡Empate!")
     elif win1==True:
