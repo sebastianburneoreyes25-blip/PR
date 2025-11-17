8d073d99a6d7997312d29a3d6d96e8b92c003a24
[sebastianburneoreyes25-blip] <[sebastian.burneoreyes25@campusfp.es]>
2025-11-17 21:06:15 +0100

    Termino de dar las ultimas pinceladas al conecta4

--- python/conecta4/j2.py
+++ python/conecta4/j2.py
@@ -4,18 +4,22 @@ import c4_core as cc
 
 def turnoJ2(tablero,tableroUser,cantidadcol, cantidaFil,win1,win2):
     jug=2
-    x=input(f"¿En que columna quieres poner tu ficha? Las columnas van del 0 al {cantidadcol-1} " \
-    "de izquierda a derecha.\n")
-    x=cc.esNumerico(x)
-    while x>=cantidadcol:
-        x=input(f"Columna fuera de rango, recuerda poner numeros del 0 al {cantidadcol-1}. Elige de nuevo\n")
-        x=cc.esNumerico
     colocado=False
-    for i in range(cantidaFil):
-        if tablero[(cantidaFil-i-1),x]==0 and colocado==False:
-            tablero[(cantidaFil-i-1),x]=2
-            tableroUser[(cantidaFil-i-1),x]="🔵"
-            colocado=True
+    while colocado==False:
+        x=input(f"¿En que columna quieres poner tu ficha? Las columnas van del 0 al {cantidadcol-1} " \
+        "de izquierda a derecha.\n")
+        x=cc.esNumerico(x)
+        while x>=cantidadcol:
+            x=input(f"Columna fuera de rango, recuerda poner numeros del 0 al {cantidadcol-1}. Elige de nuevo\n")
+            x=cc.esNumerico
+        
+        for i in range(cantidaFil):
+            if tablero[(cantidaFil-i-1),x]==0 and colocado==False:
+                tablero[(cantidaFil-i-1),x]=2
+                tableroUser[(cantidaFil-i-1),x]="🔵"
+                colocado=True
+            if colocado==False:
+                print("No se ha podido colocar la pieza, prueba de nuevo")
     win1,win2=cc.winJugador(tablero,cantidadcol,cantidaFil,jug,win1,win2)
 
     cc.mostrarTabl(tableroUser)
--- python/conecta4/main.py
+++ python/conecta4/main.py
@@ -16,15 +16,19 @@ win1=False
 win2=False
 
 
+
 #Logica de programación
 cc.bienvenida()
 
-while win1==False and win2==False:
+while win1==False and win2==False and (tablero==0).any():
     win1,win2=j1.turnoJ1(tablero,tableroUser,cantidadCol, cantidadFilas,win1,win2)
     if win1==False:
         win1,win2=j2.turnoJ2(tablero,tableroUser,cantidadCol, cantidadFilas,win1,win2)
-    elif win1==True:
+if win1==True:
         print("Felicidades jugador Rojo, ¡HAS GANADO!")
-    elif win2==True:
+elif win2==True:
         print("Felicidades jugador Azul, ¡HAS GANADO!")
+elif win1==False and win2==False and (tablero!=0).all():
+        print("Estamos ante un empate. Juega de nuevo para desempatar :)")
+
 
