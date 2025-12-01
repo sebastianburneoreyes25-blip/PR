574dfb66a413f051556b6fb2c1f07393f74c0e63
“[SebastianBurneo]” <sebastian.burneoreyes25@campusfp.es>
2025-12-01 12:53:04 +0100

    CASI CASI ESTA

--- python/damas/j1.py
+++ python/damas/j1.py
@@ -35,7 +35,7 @@ def movimientosJ1(tablero,x,y,movValido,tablerouser):
                         comprobacionPeon(x,y,tablero,tablerouser)#comprobamos que el peon coma de manera automatica si puede
                         movValido=True
 
-        if tablero[y,x]==2:#Si la casilla seleccionada tiene una dama, se realiza este código
+        if tablero[y,x]==2:#Si la casilla seleccionada tiene una dama del jugador 1, se realiza este código
             while movValido==False:
                 rangoVal=False
                 while rangoVal==False:#Hasta que el jugador no ponga un rango especifico no se podra salir del bucle
@@ -51,9 +51,39 @@ def movimientosJ1(tablero,x,y,movValido,tablerouser):
                         tablero[y+cantidad,x-cantidad]=1
                         tablerouser[y,x]='  '
                         tablerouser[y+cantidad,x-cantidad]="⚪"
-                        x=x-1
-                        y=x+1
+                        x=x-cantidad
+                        y=y+cantidad
+                        comprobacionDama(x,y,tablero,tablerouser)
 
+                if movHorizontal==1 and movVertical==1:
+                    if tablero[y+cantidad,x+cantidad]==0:
+                        tablero[y,x]=0
+                        tablero[y+cantidad,x+cantidad]=1
+                        tablerouser[y,x]='  '
+                        tablerouser[y+cantidad,x+cantidad]="⚪"
+                        x=x+cantidad
+                        y=y+cantidad
+                        comprobacionDama(x,y,tablero,tablerouser)
+
+                if movHorizontal==1 and movVertical==0:
+                    if tablero[y-cantidad,x+cantidad]==0:
+                        tablero[y,x]=0
+                        tablero[y-cantidad,x+cantidad]=1
+                        tablerouser[y,x]='  '
+                        tablerouser[y-cantidad,x+cantidad]="⚪"
+                        x=x+cantidad
+                        y=y-cantidad
+                        comprobacionDama(x,y,tablero,tablerouser)
+
+                if movHorizontal==0 and movVertical==0:
+                    if tablero[y-cantidad,x-cantidad]==0:
+                        tablero[y,x]=0
+                        tablero[y-cantidad,x-cantidad]=1
+                        tablerouser[y,x]='  '
+                        tablerouser[y-cantidad,x-cantidad]="⚪"
+                        x=x-cantidad
+                        y=y-cantidad
+                        comprobacionDama(x,y,tablero,tablerouser)
 
 
 def rangoPeon(x,y,mov):#Funcion que nos comprobará si se puede hacer el movimiento(si no se sale del tablero)
@@ -124,3 +154,73 @@ def rangoDama(x,y,movHorizontal,movVertical,cantidad):#Funcion para comprobar qu
         if movVertical==1 and y+cantidad<=7:
             rangoValido=True
     return rangoValido
+
+def comprobacionDama(x,y,tablero,tablerouser):
+    flag=False
+    while flag==False:
+        #Dependiendo de en que columna se encuentre la dama vera las posibilidades para comer, si esta en la columna 1 no podrá directamente comer hacia la izquierda por el tamaño del tablero
+        #y por el moviemiento(se mueve dos hacia abajo/arriba e izquierda/derecha)
+        if x>=2 and y+2<=7:#Podra comer en todas las direcciones diagonales
+            if tablero[y+1,x-1]>2 and tablero[y+2,x-2]==0:#Hacia abajo izq
+                tablero[y,x]=0
+                tablero[y+1,x-1]=0
+                tablero[y+2,x-2]=2
+                tablerouser[y,x]='  '
+                tablerouser[y+1,x-1]='  '
+                tablerouser[y+2,x-2]="⚪D"
+                y=y+2
+                x=x-2
+            elif tablero[y-1,x-1]>2 and tablero[y-2,x-2]==0:#Hacia arriba izq
+                tablero[y,x]=0
+                tablero[y-1,x-1]=0
+                tablero[y-2,x-2]=2
+                tablerouser[y,x]='  '
+                tablerouser[y-1,x-1]='  '
+                tablerouser[y-2,x-2]="⚪D"
+                y=y+2
+                x=x-2
+            elif tablero[y-1,x+1]>2 and tablero[y-2,x+2]==0:#Hacia arriba derecha
+                tablero[y,x]=0
+                tablero[y-1,x+1]=0
+                tablero[y-2,x+2]=2
+                tablerouser[y,x]='  '
+                tablerouser[y-1,x+1]='  '
+                tablerouser[y-2,x+2]="⚪D"
+                y=y+2
+                x=x-2
+            elif tablero[y+1,x+1]>2 and tablero[y+2,x+2]==0:#Hacia abajo derecha
+                tablero[y,x]=0
+                tablero[y+1,x-1]=0
+                tablero[y+2,x-2]=2
+                tablerouser[y,x]='  '
+                tablerouser[y+1,x-1]='  '
+                tablerouser[y+2,x-2]="⚪D"
+                y=y+2
+                x=x+2
+            else:#Si no puede comer entrara aqui para cambiar la variable flag y salir del bucle
+                flag=True
+
+        if x<2 and y+2<=7:#Por el tamaño del tablero solo puede comer en las diagonales derechas(arriba y abajo)
+            if tablero[y+1,x+1]>2 and tablero[y+2,x+2]==0:
+                tablero[y,x]=0
+                tablero[y+1,x+1]=0
+                tablero[y+2,x+2]=2
+                tablerouser[y,x]='  '
+                tablerouser[y+1,x+1]='  '
+                tablerouser[y+2,x+2]="⚪D"
+                y=y+2
+                x=x+2
+            elif tablero[y-1,x+1]>2 and tablero[y-2,x+2]==0:
+                tablero[y,x]=0
+                tablero[y-1,x+1]=0
+                tablero[y-2,x+2]=2
+                tablerouser[y,x]='  '
+                tablerouser[y-1,x+1]='  '
+                tablerouser[y-2,x+2]="⚪D"
+                y=y+2
+                x=x+2
+
+            else:#Si no puede comer entrara aqui para cambiar la variable flag y salir del bucle
+                flag=True
+        else:#Si no puede comer entrara aqui para cambiar la variable flag y salir del bucle
+            flag=True
