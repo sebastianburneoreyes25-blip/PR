f09181d0806fb6afc9a264b8af22cb5f88ae95aa
“[SebastianBurneo]” <sebastian.burneoreyes25@campusfp.es>
2026-04-24 13:38:43 +0200

    Eso

---
+++ Java/EjercicioActividadEV2/gestionTransporte/VehiculoTransporte.java
@@ -0,0 +1,12 @@
+package EjercicioActividadEV2.gestionTransporte;
+
+
+
+public abstract class VehiculoTransporte {
+    
+    int id;
+    double capacidadCarga;
+
+    public  abstract void calcularCosteRuta(double km);
+
+}
